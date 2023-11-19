7237c76823567013b40573df10238354593aea37
bulutme <bltbey06@gmail.com>
2023-11-19 15:10:35 +0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 16px;
+  }
+
+  html, body, #__next {
+    height: 100%;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style-type: none
+  }
+`;
+
+export default GlobalStyles;
